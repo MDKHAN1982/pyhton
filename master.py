ae499df17551ac47e8403205c5e3442aa0df5a9a
khan jaffar <mdkhanfem@gmail.com>
2025-09-25 09:30:29 +0300

    Add current path function and multiply implementation

--- master.py
+++ master.py
@@ -6,3 +6,12 @@ def current_path():
 current_path() 
 os.chdir('../') 
 current_path()
+
+def multiply(*args):
+    result = 1
+    for num in args:
+        result *= num
+    return result
+
+print(multiply(2, 8))
+
