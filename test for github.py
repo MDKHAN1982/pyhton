ddf6739ccc14504b1e6ac5886b27098026d65ee9
khan jaffar <mdkhanfem@gmail.com>
2025-09-25 10:29:45 +0300

    to git @10:30 9/25

--- test for github.py
+++ test for github.py
@@ -1,2 +1,14 @@
 # commented after testing
 print("Hello, World!")
+print("Enter value for x")
+x = int(input())
+if x % 3 == 0:
+     if x % 5 == 0:
+          print("Your number is divisible by 3 and 5 ")
+     else:
+          print("Your number is divisible by 3 and NOT 5")
+elif x % 5 ==0:
+    print("Your number is divisible by 5 and NOT  by 3")
+else:
+    print("Your number is NOT divisible by 3 & 5")
+print ("End of program")
